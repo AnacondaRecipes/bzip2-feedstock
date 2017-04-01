0fd6f8c4d71afd83a679ad9c23da9467660967ae
Ray Donnelly <mingw.android@gmail.com>
2017-04-02 00:35:17 +0100

    Add cross-compilation support

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,12 @@
 # toolchain flags + bzip flags + fpic
 export CFLAGS="${CFLAGS} -Wall -Winline -O2 -g -D_FILE_OFFSET_BITS=64 -fPIC"
 
-make install PREFIX=${PREFIX} CFLAGS="$CFLAGS"
+make install PREFIX=${PREFIX} CFLAGS="$CFLAGS" CC="$GCC"
 
 # build shared library
 if test `uname` = "Linux"
 then
-  make -f Makefile-libbz2_so CFLAGS="$CFLAGS"
+  make -f Makefile-libbz2_so CFLAGS="$CFLAGS" CC="$GCC"
   ln -s libbz2.so.${PKG_VERSION} libbz2.so
   cp -d libbz2.so* $PREFIX/lib/
 else  # macOS
