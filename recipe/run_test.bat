5868f7615ec4e13b840ef053e3d608b7b087b83f
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-07 11:20:59 +0200

    Rewrite the test command

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -8,7 +8,10 @@ if not exist %LIBRARY_BIN%\libbz2.dll exit 1
 if not exist %LIBRARY_LIB%\bzip2_static.lib exit 1
 if not exist %LIBRARY_LIB%\libbz2_static.lib exit 1
 
-%CC% -I%LIBRARY_PREFIX%\include -L%LIBRARY_PREFIX%\bin -lbz2 -o defaults-bz2 bz2.c
+
+%CC% /Tc bz2.c /I %CONDA_PREFIX%\Library\include /link /LIBPATH:"%CONDA_PREFIX%\Library\lib" libbz2.lib /out:defaults-bz2
+
+::%CC% -I%LIBRARY_PREFIX%\include -L%LIBRARY_PREFIX%\bin -lbz2 -o defaults-bz2 bz2.c
 if errorlevel 1 exit /B 1
 
 defaults-bz2.exe foo.bz2
