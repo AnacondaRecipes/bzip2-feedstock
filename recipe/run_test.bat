4f3b34f3a0792553427fbc02ab6be4b2394c2176
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-07 10:58:14 +0200

    Move windows tests to run_test.bat

---
+++ recipe/run_test.bat
@@ -0,0 +1,15 @@
+@echo on
+
+if not exist %LIBRARY_INC%\bzlib.h exit 1
+if not exist %LIBRARY_LIB%\bzip2.lib exit 1
+if not exist %LIBRARY_BIN%\bzip2.dll exit 1
+if not exist %LIBRARY_LIB%\libbz2.lib exit 1
+if not exist %LIBRARY_BIN%\libbz2.dll exit 1
+if not exist %LIBRARY_LIB%\bzip2_static.lib exit 1
+if not exist %LIBRARY_LIB%\libbz2_static.lib exit 1
+
+%CC% -I%LIBRARY_PREFIX%\include -L%LIBRARY_PREFIX%\bin -lbz2 -o defaults-bz2 bz2.c
+if errorlevel 1 exit /B 1
+
+defaults-bz2.exe foo.bz2
+if errorlevel 1 exit /B 1
