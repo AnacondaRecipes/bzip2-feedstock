9e2796b8f771bd02f6ec641533b4e08c6a028cbe
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-07 12:01:54 +0200

    Fix the test command

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -9,7 +9,7 @@ if not exist %LIBRARY_LIB%\bzip2_static.lib exit 1
 if not exist %LIBRARY_LIB%\libbz2_static.lib exit 1
 
 
-%CC% /Tc bz2.c /I %CONDA_PREFIX%\Library\include /link /LIBPATH:"%CONDA_PREFIX%\Library\bin" libbz2.dll /out:defaults-bz2
+%CC% /Tc bz2.c /I %CONDA_PREFIX%\Library\include /link /LIBPATH:"%CONDA_PREFIX%\Library\lib" bzip2.lib /out:defaults-bz2
 
 ::%CC% -I%LIBRARY_PREFIX%\include -L%LIBRARY_PREFIX%\bin -lbz2 -o defaults-bz2 bz2.c
 if errorlevel 1 exit /B 1
