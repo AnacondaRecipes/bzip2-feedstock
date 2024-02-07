d4b86ab0b683ab30949486a4e640e5f8b72003a2
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-07 12:48:28 +0200

    Test bzip2.exe

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -7,12 +7,8 @@ if not exist %LIBRARY_LIB%\libbz2.lib exit 1
 if not exist %LIBRARY_BIN%\libbz2.dll exit 1
 if not exist %LIBRARY_LIB%\bzip2_static.lib exit 1
 if not exist %LIBRARY_LIB%\libbz2_static.lib exit 1
+if not exist %LIBRARY_BIN%\bzip2.exe exit 1
 
 
-%CC% /Tc bz2.c /I %CONDA_PREFIX%\Library\include /link /LIBPATH:"%CONDA_PREFIX%\Library\lib" bzip2.lib /out:defaults-bz2
-
-::%CC% -I%LIBRARY_PREFIX%\include -L%LIBRARY_PREFIX%\bin -lbz2 -o defaults-bz2 bz2.c
-if errorlevel 1 exit /B 1
-
-defaults-bz2.exe foo.bz2
+bzip2.exe foo.bz2
 if errorlevel 1 exit /B 1
