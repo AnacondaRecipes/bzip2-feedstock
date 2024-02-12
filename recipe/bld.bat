2396212e1363570218a9e1445b8eaab64aca4194
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-12 11:56:44 +0200

    Add patches

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@
 REM edited from the one from Anaconda
 
 REM Build step
-jom -f makefile.msc lib bzip2
+jom -f makefile.msc all
 if errorlevel 1 exit 1
 
 
@@ -13,8 +13,8 @@ REM Some packages expect 'bzip2.lib', so make another copy
 copy libbz2.lib %LIBRARY_LIB%\bzip2_static.lib || exit 1
 copy bzlib.h %LIBRARY_INC% || exit 1
 
-cl /O2 /Ibzlib.h /Ibzlib_private.h /D_USRDLL /D_WINDLL blocksort.c bzlib.c compress.c crctable.c decompress.c huffman.c randtable.c /LD /Felibbz2.dll /link /DEF:libbz2.def
-if errorlevel 1 exit 1
+::cl /O2 /Ibzlib.h /Ibzlib_private.h /D_USRDLL /D_WINDLL blocksort.c bzlib.c compress.c crctable.c decompress.c huffman.c randtable.c /LD /Felibbz2.dll /link /DEF:libbz2.def
+::if errorlevel 1 exit 1
 
 copy libbz2.lib %LIBRARY_LIB% || exit 1
 REM Some packages expect 'bzip2.lib', so make copies
