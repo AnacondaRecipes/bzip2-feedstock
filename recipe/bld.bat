7d78d68c3048f2849b584fffbbe1cbcc189d855c
Serhii Kupriienko <skupriienko@anaconda.com>
2024-02-12 12:24:51 +0200

    Add trailing whitespaces to patches, copy libbz2.exp

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,6 +19,8 @@ copy bzlib.h %LIBRARY_INC% || exit 1
 copy libbz2.lib %LIBRARY_LIB% || exit 1
 REM Some packages expect 'bzip2.lib', so make copies
 copy libbz2.lib %LIBRARY_LIB%\bzip2.lib || exit 1
+copy libbz2.exp %LIBRARY_LIB%\libbz2.exp || exit 1
+REM Some packages expect 'bzip2.dll', so make copies
 copy libbz2.dll %LIBRARY_BIN% || exit 1
 copy libbz2.dll %LIBRARY_BIN%\bzip2.dll || exit 1
 
