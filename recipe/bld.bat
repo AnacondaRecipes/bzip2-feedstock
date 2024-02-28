42f6de662165c166ddb8af2d5e9db5686e477918
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2024-02-28 06:57:25 +0000

    bzip2 1.0.8: Rebuild on win (#3)
    * Fix license
    
    * Move windows tests to run_test.bat
    
    * Copy exes
    
    * Test bzip2.exe
    
    * Add trailing whitespaces to patches, copy libbz2.exp
    
    * Add one patch
    
    * Set BZIP2/BZIP
    
    * Copy LIBBZ2.dll to LIBRARY_BIN
    
    * Add abs.yaml
    
    * Add /MD to link with MSVCRT.lib
    
    * Update recipe/bld.bat
    
    Co-authored-by: Ian Fitchet <123584373+ifitchet@users.noreply.github.com>
    
    * Add --no-error-overlinking to abs.yaml
    
    * Add the reason to not use --error-overlinking
    
    * Add a comment about build_dll.patch
    
    * Modify:
    - Add a notice about the origin of the patch and its license.
    
    - Bump the build number to 5 because osx-arm64 has the number 4.
    
    - Add CFLAGS to build.sh and LDFLAGS for osx-*.
    
    - Add a comment about the license name
    
    * Update recipe/meta.yaml
    
    Co-authored-by: Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>
    
    * Update recipe/meta.yaml
    
    Co-authored-by: Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>
    
    * Update recipe/meta.yaml
    
    Co-authored-by: Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>
    
    * Remove dot
    
    * Test executables correctly
    
    * Remove the selector
    
    ---------
    
    Co-authored-by: Ian Fitchet <123584373+ifitchet@users.noreply.github.com>
    Co-authored-by: Jean-Christophe Morin <38703886+JeanChristopheMorinPerso@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,32 @@
-REM edited from the one from Anaconda
+@echo on
+
+REM The bzip2 executable uses the environment variables BZIP2 and BZIP to initialise its ArgList
+REM conda sets these values from conda_build_config.yaml and bzip2 tries to open a file called "1.0"
+set BZIP2=
+set BZIP=
 
 REM Build step
-jom -f makefile.msc lib bzip2
+nmake -f makefile.msc
 if errorlevel 1 exit 1
 
 REM Install step
-copy libbz2.lib %LIBRARY_LIB%\libbz2_static.lib || exit 1
-REM Some packages expect 'bzip2.lib', so make another copy
-copy libbz2.lib %LIBRARY_LIB%\bzip2_static.lib || exit 1
 copy bzlib.h %LIBRARY_INC% || exit 1
-
-cl /O2 /Ibzlib.h /Ibzlib_private.h /D_USRDLL /D_WINDLL blocksort.c bzlib.c compress.c crctable.c decompress.c huffman.c randtable.c /LD /Felibbz2.dll /link /DEF:libbz2.def
-if errorlevel 1 exit 1
+copy libbz2_static.lib %LIBRARY_LIB% || exit 1
+REM Some packages expect 'bzip2.lib', so make another copy
+copy libbz2_static.lib %LIBRARY_LIB%\bzip2_static.lib || exit 1
 
 copy libbz2.lib %LIBRARY_LIB% || exit 1
 REM Some packages expect 'bzip2.lib', so make copies
 copy libbz2.lib %LIBRARY_LIB%\bzip2.lib || exit 1
+copy libbz2.exp %LIBRARY_LIB%\libbz2.exp || exit 1
+copy libbz2.def %LIBRARY_LIB%\libbz2.def || exit 1
+
 copy libbz2.dll %LIBRARY_BIN% || exit 1
+REM Some packages expect 'bzip2.dll', so make copies
 copy libbz2.dll %LIBRARY_BIN%\bzip2.dll || exit 1
+
+REM Copy also uppercased variant of the dll
+copy LIBBZ2.dll %LIBRARY_BIN%\LIBBZ2.dll || exit 1
+
+REM Copy exe files
+copy *.exe %LIBRARY_BIN% || exit 1
